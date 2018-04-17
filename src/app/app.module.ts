23223bc4036e9b30102466fecf1f623b296cbb41
Volodymyr Shevchuk <Volodymyr.Shevchuk@eleks.com>
2018-04-17 19:44:16 +0300

    Added wrappers for applications;

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { ReportsWrapperComponent } from './reports-wrapper/reports-wrapper.component';
+import { OverallComponent } from './overall/overall.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    ReportsWrapperComponent
+    ReportsWrapperComponent,
+    OverallComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: '', pathMatch: 'full', component: OverallComponent },
+      {
+        path: 'core',
+        loadChildren: './core-wrapper/core-wrapper.module#CoreWrapperModule'
+      },
+      {
+        path: 'reports',
+        component: ReportsWrapperComponent
+      }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
